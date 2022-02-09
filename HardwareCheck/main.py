dc67f60980605424e64b018ec7f282e83350f48f
bubbles91 <john.lovelace00@gmail.com>
2022-02-09 08:59:37 -0500

    Added comments to the code

--- HardwareCheck/main.py
+++ HardwareCheck/main.py
@@ -6,15 +6,17 @@ import tkinter.messagebox as tkmb
 import wmi
 
 def write_slogan():
+    ##Sets up WMI and variables
     c = wmi.WMI()
     systeminfogpu = c.Win32_VideoController()[0]
     systeminfoboard = c.Win32_BaseBoard()[0]
+## Gathers the information from the computer and creates a message to display on the pop up box
     message=str((f"Computer Name: {platform.node()}\n") + str(f"Platform: {platform.platform()}\n") +
         str(f'CPU Brand Info: {cpuinfo.get_cpu_info()["brand_raw"]}\n') + str(f"CPU cores: {psutil.cpu_count()}\n") +
         str(f"Ram installed: {math.ceil(psutil.virtual_memory().total/1073741824)} GB\n") +
         str(f"GPU: {systeminfogpu.Name}\n") + str(f"MotherBoard: {systeminfoboard.Product} {systeminfoboard.Manufacturer}\n"))
 
-
+##Create the output message with TKinter
     tkmb.showinfo("Output", message)
 
 write_slogan()
